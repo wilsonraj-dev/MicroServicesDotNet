8adb25e02b3631a91f8d6faa6893163057c6a573
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-10-03 22:05:37 -0300

    Creating IProductRepository

--- GeekShooping/GeekShopping.Product.API/Repository/IProductRepository.cs
+++ GeekShooping/GeekShopping.Product.API/Repository/IProductRepository.cs
@@ -1,7 +1,13 @@
-﻿namespace GeekShopping.Product.API.Repository
+﻿using GeekShopping.Product.API.DTO;
+
+namespace GeekShopping.Product.API.Repository
 {
     public interface IProductRepository
     {
-
+        Task<IEnumerable<ProductDTO>> FindAllAsync();
+        Task<ProductDTO> FindByIdAsync(long id);
+        Task<ProductDTO> CreateAsync(ProductDTO productDTO);
+        Task<ProductDTO> UpdateAsync(ProductDTO productDTO);
+        Task<bool> DeleteAsync(long id);
     }
 }
