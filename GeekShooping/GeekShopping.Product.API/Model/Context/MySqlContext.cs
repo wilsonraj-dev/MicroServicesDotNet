fa3dc02d0f90ec95df93d54faf6d428dd4a239dd
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-10-03 21:38:44 -0300

    Mapping databases entity

--- GeekShooping/GeekShopping.Product.API/Model/Context/MySqlContext.cs
+++ GeekShooping/GeekShopping.Product.API/Model/Context/MySqlContext.cs
@@ -7,5 +7,7 @@ namespace GeekShopping.Product.API.Model.Context
         public MySqlContext() { }
 
         public MySqlContext(DbContextOptions<MySqlContext> options) : base(options) { }
+
+        public DbSet<Product>? Products { get; set; }
     }
 }
---
+++ GeekShooping/GeekShopping.Product.API/Model/Product.cs
@@ -0,0 +1,32 @@
+﻿using GeekShopping.Product.API.Model.Base;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.Product.API.Model
+{
+    [Table("Products")]
+    public class Product : BaseEntity
+    {
+        [Column("name")]
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; } = string.Empty;
+
+        [Column("price")]
+        [Required]
+        [Range(1, 100000)]
+        public decimal Price { get; set; }
+
+        [Column("description")]
+        [StringLength(500)]
+        public string Description { get; set; } = string.Empty;
+
+        [Column("category_name")]
+        [StringLength(50)]
+        public string CategoryName { get; set; } = string.Empty;
+
+        [Column("image_url")]
+        [StringLength(300)]
+        public string ImageURL { get; set; } = string.Empty;
+    }
+}
