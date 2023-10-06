bc5041cbce06b21a4fa3d36abce956bab2f74da3
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-10-05 23:05:06 -0300

    Creating ProductRepository class

--- GeekShooping/GeekShopping.Product.API/Program.cs
+++ GeekShooping/GeekShopping.Product.API/Program.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using GeekShopping.Product.API.DTO.Mappings;
 using GeekShopping.Product.API.Model.Context;
+using GeekShopping.Product.API.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +16,8 @@ builder.Services.AddDbContext<MySqlContext>(options =>
 IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
 builder.Services.AddSingleton(mapper);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ GeekShooping/GeekShopping.Product.API/Repository/ProductRepository.cs
@@ -0,0 +1,72 @@
+﻿using AutoMapper;
+using GeekShopping.Product.API.DTO;
+using GeekShopping.Product.API.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.Product.API.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly MySqlContext _context;
+        private readonly IMapper _mapper;
+
+        public ProductRepository(MySqlContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<ProductDTO>> FindAllAsync()
+        {
+            List<Model.Product> products = await _context.Products.ToListAsync();
+            return _mapper.Map<List<ProductDTO>>(products);
+        }
+
+        public async Task<ProductDTO> FindByIdAsync(long id)
+        {
+            Model.Product product = await _context.Products.Where(p => p.Id == id).FirstOrDefaultAsync();
+            return _mapper.Map<ProductDTO>(product);
+        }
+
+        public async Task<ProductDTO> CreateAsync(ProductDTO productDTO)
+        {
+            Model.Product product = _mapper.Map<Model.Product>(productDTO);
+
+            _context.Products?.Add(product);
+            await _context.SaveChangesAsync();
+            return _mapper.Map<ProductDTO>(product);
+        }
+
+        public async Task<ProductDTO> UpdateAsync(ProductDTO productDTO)
+        {
+            Model.Product product = _mapper.Map<Model.Product>(productDTO);
+
+            _context.Products?.Update(product);
+            await _context.SaveChangesAsync();
+            return _mapper.Map<ProductDTO>(product);
+        }
+
+        public async Task<bool> DeleteAsync(long id)
+        {
+            try
+            {
+                Model.Product product = await _context.Products.Where(p => p.Id == id).FirstOrDefaultAsync();
+
+                if (product == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    _context.Products.Remove(product);
+                    await _context.SaveChangesAsync();
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
