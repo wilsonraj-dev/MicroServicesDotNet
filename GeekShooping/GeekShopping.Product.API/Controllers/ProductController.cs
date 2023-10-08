9e8dbb08688f0c14029a95ad39102d16e6579b59
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-10-08 17:07:37 -0300

    Creating POST, PUT and DELETE

--- GeekShooping/GeekShopping.Product.API/Controllers/ProductController.cs
+++ GeekShooping/GeekShopping.Product.API/Controllers/ProductController.cs
@@ -41,5 +41,46 @@ namespace GeekShopping.Product.API.Controllers
 
             return Ok(product);
         }
+
+        [HttpPost]
+        public async Task<ActionResult<ProductDTO>> Create([FromBody] ProductDTO product)
+        {
+            if (product == null)
+            {
+                return BadRequest();
+            }
+
+            var p = await _productRepository.CreateAsync(product);
+            return Ok(p);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<ProductDTO>> Update(int id, [FromBody] ProductDTO product)
+        {
+            if (product == null)
+            {
+                return NotFound();
+            }
+            else if (product.Id != id)
+            {
+                return BadRequest();
+            }
+
+            var p = await _productRepository.UpdateAsync(product);
+            return Ok(p);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(long id)
+        {
+            var status = await _productRepository.DeleteAsync(id);
+
+            if (!status)
+            {
+                return BadRequest();
+            }
+
+            return Ok(status);
+        }
     }
 }
