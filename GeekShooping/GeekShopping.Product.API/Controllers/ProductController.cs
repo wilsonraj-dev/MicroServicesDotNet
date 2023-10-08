3bf97741ae53ee996a214b2fa512a94331940202
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-10-08 16:56:38 -0300

    Creating GET

---
+++ GeekShooping/GeekShopping.Product.API/Controllers/ProductController.cs
@@ -0,0 +1,45 @@
+﻿using GeekShopping.Product.API.DTO;
+using GeekShopping.Product.API.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.Product.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> Get()
+        {
+            var product = await _productRepository.FindAllAsync();
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductDTO>> GetById(long id)
+        {
+            var product = await _productRepository.FindByIdAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+    }
+}
